2e61e73089370f0d01b4c13ab85cdef9752f2254
Muhammad Arslan <arslanarif8088@ucp.edu.pk>
2022-04-21 17:33:13 +0500

    profile section done

--- src/stores/auth.store.js
+++ src/stores/auth.store.js
@@ -3,25 +3,31 @@ import {
 } from 'pinia'
 
 import {
-  Cookies
-} from 'quasar'
-
-import {
   Notify
 } from 'quasar'
 import API from 'src/services/API';
 import redirect from 'src/helpers/redirect';
 
+const getDeviceId = () => {
+  const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (var i = 0; i < 19; i++) {
+    result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result + Date.now();
+}
+
 export const useAuthStore = defineStore('authStore', {
   state: () => ({
     authUser: null,
-    registerUser: null
+    authToken: null,
+    deviceId: null,
   }),
-  getters: {
-    getAuthUser(state) {
-      return state.authUser
-    },
-  },
+  // getters: {
+  //   getAuthUser(state) {
+  //     return state.authUser
+  //   },
+  // },
   actions: {
     async login(creds) {
 
@@ -29,8 +35,8 @@ export const useAuthStore = defineStore('authStore', {
 
       if (response.status == 200) {
         this.authUser = response.data.user;
-        Cookies.set('AUTH_USER', response.data.user);
-        Cookies.set('1TIMEUSED_TOKEN', response.data.token);
+        this.$cookies.set('AUTH_USER', response.data.user);
+        this.$cookies.set('1TIMEUSED_TOKEN', response.data.token);
 
         Notify.create({
           message: response.message,
@@ -57,8 +63,7 @@ export const useAuthStore = defineStore('authStore', {
       const response = await API.post('register', creds);
 
       if (response.status == 200) {
-        this.registerUser = response.data;
-        Cookies.set('REGISTER_NEW_USER', response.data);
+        this.authUser = response.data;
 
         Notify.create({
           message: response.message,
@@ -81,11 +86,43 @@ export const useAuthStore = defineStore('authStore', {
     },
 
     loadAuthUser() {
-      if (!this.authUser) {
-        this.authUser = this.$cookies.get('AUTH_USER')
-        console.log("sdfsadsf", this.authUser)
+      const deviceIdKey = '1TIMUSED_DEVICE_ID';
+      if (!this.$cookies.get(deviceIdKey)) {
+        this.$cookies.set(deviceIdKey, getDeviceId(32))
       }
-    }
+
+      this.deviceId = this.$cookies.get(deviceIdKey);
+      this.authToken = this.$cookies.get('1TIMEUSED_TOKEN');
+      this.authUser = this.$cookies.get('AUTH_USER')
+    },
+
+    async updateProfile(creds) {
+
+      const response = await API.put('profile', creds);
+
+      console.log(response.data);
+      if (response.status == 200) {
+        this.authUser = response.data;
+        this.$cookies.set('AUTH_USER', response.data);
+
+        Notify.create({
+          message: response.message,
+          icon: 'done',
+          position: 'bottom',
+          color: 'success',
+        })
+        //redirect('');
+
+      } else {
+        Notify.create({
+          message: response.message,
+          icon: 'warning',
+          position: 'bottom',
+          color: 'negative',
+        })
+      }
+      return response;
+    },
 
   },
 })
