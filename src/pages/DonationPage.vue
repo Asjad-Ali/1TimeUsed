0bc63aa756ae2422e340f6e548ed1ca9e45093d2
unknown <Asif.ali14280@gmail.com>
2022-04-30 14:10:12 +0500

    fixis

--- src/pages/DonationPage.vue
+++ src/pages/DonationPage.vue
@@ -1,64 +1,69 @@
 <template>
   <div class="container">
-    <div class="column q-pb-xl">
-      <div class="row justify-between items-center q-mt-lg">
-        <div class="col-md-3 col-6">
-          <q-icon
-            name="grid_view"
-            color="primary"
-            size="sm"
-            class="cursor-pointer mb-10-mobile"
-          />
-        </div>
-        <div class="col-md-6 col-12">
-          <q-select
-            filled
-            v-model="search"
-            use-input
-            hide-selected
-            fill-input
-            input-debounce="400"
-            dense
-            @update:model-value="searchProducts"
-            label="Search Here"
-            @filter="getSearchSuggestions"
-            :options="searchSuggestions"
-            @filter-abort="searchProducts"
-            style="max-width: 100%"
-            dropdown-icon="false"
-          >
-            <template v-slot:no-option>
-              <q-item>
-                <q-item-section class="text-grey"> No results</q-item-section>
-              </q-item>
-            </template>
+    <div class="row justify-between items-center q-mt-lg">
+      <div class="col-md-3 col-6">
+        <q-icon
+          name="grid_view"
+          color="primary"
+          size="sm"
+          class="cursor-pointer mb-10-mobile"
+        />
+      </div>
+      <div class="col-md-6 col-12">
+        <q-select
+          filled
+          v-model="search"
+          use-input
+          hide-selected
+          fill-input
+          input-debounce="400"
+          dense
+          @update:model-value="searchProducts"
+          label="Search Here"
+          @filter="getSearchSuggestions"
+          :options="searchSuggestions"
+          @filter-abort="searchProducts"
+          style="max-width: 100%"
+          dropdown-icon="false"
+        >
+          <template v-slot:no-option>
+            <q-item>
+              <q-item-section class="text-grey"> No results</q-item-section>
+            </q-item>
+          </template>
 
-            <template v-slot:after>
-              <q-btn @click="searchProducts" icon="search" size="16px" outline>
-              </q-btn>
-            </template>
-          </q-select>
-          <h6 v-show="search" class="text-center text-grey q-my-md">
-            Results for "<span class="text-bold">{{ search }}</span
-            >"
-          </h6>
-        </div>
-
-        <div class="col-md-2 col-12">
-          <q-select
-            dense
-            outlined
-            v-model="model"
-            :options="searchSuggestions"
-            label="Sort By"
-            class="bg-white"
-          />
-        </div>
+          <template v-slot:after>
+            <q-btn @click="searchProducts" icon="search" size="16px" outline>
+            </q-btn>
+          </template>
+        </q-select>
+        <h6 v-show="search" class="text-center text-grey q-my-md">
+          Results for "<span class="text-bold">{{ search }}</span
+          >"
+        </h6>
       </div>
 
+      <div class="col-md-2 col-12">
+        <q-select
+          dense
+          outlined
+          v-model="model"
+          :options="searchSuggestions"
+          label="Sort By"
+          class="bg-white"
+        />
+      </div>
+    </div>
+
+    <div class="column q-pb-xl">
       <h5 class="text-center">Donate Products</h5>
       <div
-        class="flex q-gutter-y-md q-gutter-x-sm q-mx-auto q-mb-lg justify-center items-center"
+        class="
+          flex
+          q-gutter-y-md q-gutter-x-sm q-mx-auto q-mb-lg
+          justify-center
+          items-center
+        "
       >
         <div
           class="q-card product-card cursor-pointer my-card"
@@ -124,7 +129,16 @@
 
           <!-- donation badge -->
           <div
-            class="q-badge flex inline items-center no-wrap q-badge--single-line bg-positive donation-baadge"
+            class="
+              q-badge
+              flex
+              inline
+              items-center
+              no-wrap
+              q-badge--single-line
+              bg-positive
+              donation-baadge
+            "
             role="alert"
           >
             {{ donateProduct.purpose }}
