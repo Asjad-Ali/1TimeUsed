c046379563f7fd9ddf216802ecdf60d8ff3242f5
unknown <Asif.ali14280@gmail.com>
2022-05-11 18:53:47 +0500

    chges

--- src/pages/SubCategoryProduct.vue
+++ src/pages/SubCategoryProduct.vue
@@ -20,7 +20,10 @@
             class="icons"
           />
         </div>
-        <div class="col text-center border-right">
+        <div
+          @click="sortModal = true"
+          class="col text-center border-right cursor-pointer"
+        >
           <q-icon
             name="filter_list"
             :size="$q.screen.lt.md ? 'sm' : `md`"
@@ -60,6 +63,38 @@
         <TileProduct v-else :product="product" mainDIv="recent-products" />
       </div>
     </div>
+    <!-- Sort by Modal -->
+    <q-dialog v-model="sortModal">
+      <q-card style="width: 300px">
+        <q-card-section>
+          <div class="text-h6 text-center">Sort By</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <div class="flex items-center justify-between">
+            <p>Newest First</p>
+            <q-radio v-model="shape" val="line1" />
+          </div>
+          <div class="flex items-center justify-between">
+            <p>Oldest First</p>
+            <q-radio v-model="shape" val="line2" />
+          </div>
+          <div class="flex items-center justify-between">
+            <p>Price Low to High</p>
+            <q-radio v-model="shape" val="line3" />
+          </div>
+          <div class="flex items-center justify-between">
+            <p>Price High to Low</p>
+            <q-radio v-model="shape" val="line4" />
+          </div>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <!-- Sort by Modal End -->
   </div>
 </template>
 
@@ -72,6 +107,7 @@ import TileProduct from "../components/TileProduct.vue";
 
 const store = useProductStore();
 const route = useRoute();
+const sortModal = ref(false);
 const viewType = ref("grid");
 onMounted(() => store.loadSubCategoryProduct(route.params.id));
 </script>
